6d62634f474ff35d6ea99a9b3ed1295052ed071e
Raimon <rfolch2@institutmvm.cat>
2016-05-04 18:50:48 +0200

    prova

--- VMostrarAlumnes.php
+++ VMostrarAlumnes.php
@@ -8,47 +8,47 @@
 
 include_once 'Vista.php';
 
-class VMostrarAlumnes implements Vista{
+class VMostrarAlumnes extends Vista{
 	private $model;
 	
 	function __construct($model) {
 		$this->model = $model;
 	}
-
-	public function mostraPagina(){
-		?>
-		<!DOCTYPE html>
-		<html lang="es">
-			<head>
-				<meta charset="UTF-8" />
-				<title>Llistat alumnes</title>
-			</head>
-			<body>
-				<h1>Gestió de l'alumnat</h1>
-				<h2>Llistat d'alumnes</h2>
-			<?php
-				/*
-					Invoquem del model la funció que ens retorna la llista dels 
-					alumnes amb nom i cognoms.
-				*/
-				$arrayLlistat = $this->model->selectNomCognomAlumnes();
-				if(count($arrayLlistat)==0) echo "<h2> No hi ha alumnes a la base de dades </h2>";
-				else{
-					echo '<table border="1"> <th>Nom</th><th>Cognoms</th>';
+	
+	/*
+		Mètode que cal sobreescriure obligatòriament ja que és abstracte
+		a la classe Vista de la que hereta aquest classe.
+		Cal que mostri (echo) el contingut html específic d'aquesta 
+		funcionalitat.
+	*/
+	protected function mostraContingutPagina(){
+		/*
+			Invoquem, del model, la funció que ens retorna la llista dels 
+			alumnes amb nom i cognoms ho mostrem tot en una taula html.
+		*/
+		$arrayLlistat = $this->model->selectNomCognomAlumnes();
+		if(count($arrayLlistat)==0) echo "<h2> No hi ha alumnes a la base de dades </h2>";
+		else{
+			echo '<table border="1"> <th>Nom</th><th>Cognoms</th>';
 					
-					foreach($arrayLlistat as $ar)
-						echo "<tr><td>" . $ar["nom"] . "</td><td>". $ar["cognoms"] ."</td></tr>";
-						
-					
-					echo "</table>";
-				}
+			foreach($arrayLlistat as $ar)
+				echo "<tr><td>" . $ar["nom"] . "</td><td>". $ar["cognoms"] ."</td></tr>";
+	
+			echo "</table>";
+		}
 
-			?>
-				<a href="./index.php"><--Inici</a>
-			</body>
-		</html>
-		<?php
 	}
+
+	/*
+		Mètode que cal sobreescriure obligatòriament ja que és abstracte
+		a la classe Vista de la que hereta aquest classe.
+		Cal que retorni (return) un string que representarà el títol de la
+		pàgina web.
+	*/
+	protected function getTitolPaginaWeb(){
+		return "Llistat d'alumnes";
+	}	
+	
 }
 ?>
 
